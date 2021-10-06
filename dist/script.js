abe805d8cdfc07266490645b14839b9376ac9622
eteimz <8teims@gmail.com>
2021-10-06 15:26:26 +0100

    Initial commit

---
+++ dist/script.js
@@ -0,0 +1,25 @@
+// Prefer camera resolution nearest to 1280x720.
+var constraints = { video: { facingMode: { ideal: "environment" } } };
+const canvas = document.querySelector('canvas');
+const Img = document.querySelector('img');
+var snap_btn = document.querySelector('.snap');
+var video = document.querySelector('video');
+
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+
+Screenshot = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  Img.src = canvas.toDataURL('image/png');
+};
+
+snap_btn.addEventListener('click', Screenshot)
